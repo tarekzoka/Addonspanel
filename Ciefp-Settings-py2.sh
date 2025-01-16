97c6573122258948788ff1350c771b43d28d7f68
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2025-01-16 02:54:26 +0200

    Add files via upload

---
+++ Ciefp-Settings-py2.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+cd /tmp
+set -e
+ wget "https://raw.githubusercontent.com/tarekzoka/Addonspanel/main/py2/SmartAddonspanel.tar.gz"
+wait
+tar -xzf SmartAddonspanel.tar.gz  -C /
+wait
+cd ..
+set +e
+rm -f /tmp/SmartAddonspanel.tar.gz
+sleep 2;
+echo "" 
+echo "" 
+echo "*********************************************************"
+echo "*                   INSTALLED SUCCESSFULLY              *"
+echo "*                       ON - Panel                      *"
+echo "*                Enigma2 restart is required            *"
+echo "*********************************************************"
+echo "               UPLOADED BY  >>>>  tarek-hanfy           "
+sleep 4;
+	echo '================================================='
+################################################################                                                                                                                  
+echo ". >>>>         your Device will RESTART Now          <<<<"
+echo "*********************************************************"
+wait
+killall -9 enigma2
+exit 0
